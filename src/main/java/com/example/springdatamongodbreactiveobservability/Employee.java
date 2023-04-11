5d3c17280c5ec47e4eed39240506a9bb4ad9e05c
Greg L. Turnquist <gturnquist@vmware.com>
2023-04-11 16:30:59 -0500

    Polish.
    Remove usage of Testcontainers for starting up any remote services. User is responsible for having MongoDB and Zipkin up and running.

--- src/main/java/com/example/springdatamongodbreactiveobservability/Employee.java
+++ src/main/java/com/example/springdatamongodbreactiveobservability/Employee.java
@@ -41,6 +41,7 @@ public class Employee {
 
 	@Override
 	public boolean equals(Object o) {
+
 		if (this == o)
 			return true;
 		if (o == null || getClass() != o.getClass())
--- src/main/java/com/example/springdatamongodbreactiveobservability/SpringDataMongodbReactiveObservabilityApplication.java
+++ src/main/java/com/example/springdatamongodbreactiveobservability/SpringDataMongodbReactiveObservabilityApplication.java
@@ -1,25 +1,17 @@
 package com.example.springdatamongodbreactiveobservability;
 
+import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.tracing.Tracer;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.observation.ObservationRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.observability.EnableMongoObservability;
-import org.springframework.data.mongodb.observability.MongoMetricsReactiveConfigurationHelper;
+import org.springframework.data.mongodb.observability.ContextProviderFactory;
 import org.springframework.data.mongodb.observability.MongoObservationCommandListener;
-import org.springframework.data.mongodb.observability.MongoTracingObservationHandler;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.utility.DockerImageName;
-
-import com.mongodb.ConnectionString;
 
 @SpringBootApplication
-@EnableMongoObservability
 public class SpringDataMongodbReactiveObservabilityApplication {
 
 	public static void main(String[] args) {
@@ -28,40 +20,26 @@ public class SpringDataMongodbReactiveObservabilityApplication {
 
 	// Setting up MongoDB
 
-	@Bean(initMethod = "start", destroyMethod = "stop")
-	MongoDBContainer mongoDBContainer() {
-		return new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
-	}
-
 	@Bean
-	CommandLineRunner initDatabase(EmployeeRepository repository) {
+	CommandLineRunner initDatabase(EmployeeRepository repository, ObservationRegistry registry) {
+
 		return args -> {
-			repository.save(new Employee("Frodo", "ring bearer")).block();
-			repository.save(new Employee("Bilbo", "burglar")).block();
+			Observation.createNotStarted("init-database", registry).observe(() -> {
+				repository.deleteAll().block();
+				repository.save(new Employee("Frodo", "ring bearer")).block();
+				repository.save(new Employee("Bilbo", "burglar")).block();
+			});
 		};
 	}
 
 	// Configuring MongoDB + Micrometer
 
 	@Bean
-	MongoClientSettingsBuilderCustomizer mongoMetricsReactiveContextProvider(ObservationRegistry registry) {
+	MongoClientSettingsBuilderCustomizer mongoMetricsSynchronousContextProvider(ObservationRegistry registry) {
+
 		return (clientSettingsBuilder) -> {
-			clientSettingsBuilder.contextProvider( //
-					MongoMetricsReactiveConfigurationHelper.reactiveContextProvider(registry));
+			clientSettingsBuilder.contextProvider(ContextProviderFactory.create(registry))
+					.addCommandListener(new MongoObservationCommandListener(registry));
 		};
-	}
-
-	@Bean
-	MongoClientSettingsBuilderCustomizer mongoObservationCommandListenerCustomizer(MongoDBContainer mongoDBContainer,
-			MongoObservationCommandListener commandListener) {
-		return (clientSettingsBuilder) -> clientSettingsBuilder //
-				.applyConnectionString(new ConnectionString(mongoDBContainer.getConnectionString())) //
-				.addCommandListener(commandListener);
-	}
-
-	@Bean
-	ObservationRegistryCustomizer<ObservationRegistry> mongoTracingHandlerCustomizer(
-			MongoTracingObservationHandler handler) {
-		return handler::register;
 	}
 }
